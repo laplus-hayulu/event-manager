b9d6539d267f210e71f9ed4318018e7bd75d31fa
manab oz <staff@oz-edu.com>
2022-07-01 16:51:14 +0900

    edit App.js

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,6 +1,11 @@
 import React  from 'react';
 import Editor from './Editor';
 
-const App = () => <Editor />;
+const App = () => (
+  <Routes>
+    // ブラウザでevents/で始まるURLの表示を開始するたびにレンダリング
+    <Route path="events/*" element={<Editor />} />
+  <Routes>
+);
 
-export default App; 
+export default App;
