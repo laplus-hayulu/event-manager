78c1f1e50fd102513f1d177c24599645c73e049a
manab oz <staff@oz-edu.com>
2022-07-01 16:49:13 +0900

    edit application.js

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root to: redirect('/events') # http://localhost:3000/events へのrootルーティング
 
-  get 'events', to: 'site#index'
-  get 'events/new', to: 'site#index'
-  get 'events/:id', to: 'site#index'
+  get 'events',          to: 'site#index'
+  get 'events/new',      to: 'site#index'
+  get 'events/:id',      to: 'site#index'
   get 'events/:id/edit', to: 'site#index'
 
   namespace :api do
